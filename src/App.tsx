5d2c77a3b2bc9dd335fd267cb250106c8d2d01f8
Chrystian Santos <chrystiansantos.dev@MacBook-Pro.local>
2020-08-06 00:29:58 -0300

    estilizando a pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 
+import SignIn from './pages/signin';
+
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <h1>Ola mundo</h1>
+    <SignIn />
   </>
 );
 
