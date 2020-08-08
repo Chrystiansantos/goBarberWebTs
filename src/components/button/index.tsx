8ce839f5bcb3b63ef94cfd9afd06b6c00ff9ded4
Chrystian Santos <chrystiansantos.dev@MacBook-Pro.local>
2020-08-08 14:54:21 -0300

    criando component para button e input

---
+++ src/components/button/index.tsx
@@ -0,0 +1,14 @@
+import React, { ButtonHTMLAttributes } from 'react';
+
+import { Container } from './styles';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
+  /* rest é a mesma coisa que as props */
+  <Container type="button" {...rest}>
+    {children}
+  </Container>
+);
+
+export default Button;
