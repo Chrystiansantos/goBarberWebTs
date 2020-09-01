2a3881fcf34f8c7aec24035545a1f200f683cad9
Chrystian Santos <chrystiansantos.dev@MacBook-Pro.local>
2020-09-01 00:15:10 -0300

    Adicionando e removendo Toast

--- src/hooks/ToastContext.tsx
+++ src/hooks/ToastContext.tsx
@@ -1,26 +1,45 @@
-import React, { createContext, useContext, useCallback } from 'react';
-
+import React, { createContext, useContext, useCallback, useState } from 'react';
+import { uuid } from 'uuidv4';
 import ToastContainer from '../components/ToastContainer';
 
+export interface ToastMessage {
+  id: string;
+  type?: 'success' | 'error' | 'info';
+  title: string;
+  description: string;
+}
 interface ToastContextData {
-  addToast(): void;
-  removeToast(): void;
+  addToast(messages: Omit<ToastMessage, 'id'>): void;
+  removeToast(id: string): void;
 }
 
 const ToastContext = createContext<ToastContextData>({} as ToastContextData);
 
 const ToastProvider: React.FC = ({ children }) => {
-  const addToast = useCallback(() => {
-    console.log('Add toast');
-  }, []);
-  const removeToast = useCallback(() => {
-    console.log('Remove toast');
+  const [messages, setMessages] = useState<ToastMessage[]>([]);
+
+  const addToast = useCallback(
+    ({ type, title, description }: Omit<ToastMessage, 'id'>) => {
+      const id = uuid();
+      const toast = {
+        id,
+        type,
+        title,
+        description,
+      };
+      // oldMessage sao os valores antigos que haviam no messages
+      setMessages(oldMessage => [...oldMessage, toast]);
+    },
+    [],
+  );
+  const removeToast = useCallback((id: string) => {
+    setMessages(state => state.filter(message => message.id !== id));
   }, []);
 
   return (
     <ToastContext.Provider value={{ addToast, removeToast }}>
       {children}
-      <ToastContainer />
+      <ToastContainer messages={messages} />
     </ToastContext.Provider>
   );
 };
--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -47,7 +47,11 @@ const SignIn: React.FC = () => {
           formRef.current?.setErrors(erros);
         }
         // Disparar um toast
-        addToast();
+        addToast({
+          type: 'error',
+          title: 'Erro na Autenticacao',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais ',
+        });
       }
     },
     [signIn, addToast],
